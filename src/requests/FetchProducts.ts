76406c12222c6a541b3014cd0bdcc1dabe03f09b
antibioticvz@gmail.com <antibioticvz@gmail.com>
2022-04-14 23:32:43 -0500

    ✨ add suspend loading

---
+++ src/requests/FetchProducts.ts
@@ -0,0 +1,29 @@
+import { API } from 'constants/apiCall'
+
+import { suspend } from './suspend'
+
+export interface IProduct {
+  avatar: string
+  category: string
+  createdAt: number
+  description: string
+  developerEmail: string
+  id: string
+  name: string
+  price: string
+}
+
+const FetchProducts = async (): Promise<IProduct[]> => {
+  const res = await fetch(`${API}/products`, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+
+  return res.json()
+}
+
+export default suspend(FetchProducts())
