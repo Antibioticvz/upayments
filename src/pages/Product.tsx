c463e79f1a91bbb6f4619b3f0f0975a80f764291
antibioticvz@gmail.com <antibioticvz@gmail.com>
2022-04-14 23:32:06 -0500

    💄 add dummy components

---
+++ src/pages/Product.tsx
@@ -0,0 +1,3 @@
+import { FC } from 'react'
+
+export const Product: FC = () => <div>Product</div>
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import { Home } from './Home'
+import { Product } from './Product'
+import { Create } from './Create'
+import { NoMatch } from './NoMatch'
+
+export { Home, Product, Create, NoMatch }
