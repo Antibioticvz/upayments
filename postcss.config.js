a25d65b6caa3f69f136a7340a15eff22db2907d1
antibioticvz@gmail.com <antibioticvz@gmail.com>
2022-04-14 19:36:43 -0500

    ⚡️ add tailwind watch and reload

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,14 @@
-const tailwindcss = require("tailwindcss")
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+const tailwindcss = require('tailwindcss')
 
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.js"),
-    require("autoprefixer"),
-    require("@fullhuman/postcss-purgecss")({
-      content: ["./src/**/*.js", "./public/index.html"],
+    tailwindcss('./tailwind.js'),
+    require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./src/**/*.tsx', './public/index.html'],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
   ],
--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import 'assets/main.css'
 import 'assets/tailwind.css'
 
 const App: FC = () => (
-  <div className="">
-    <header className="">Learn React</header>
+  <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="px-6 py-4">
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2 ml-20">
+        UPayments
+      </span>
+    </div>
   </div>
 )
 
